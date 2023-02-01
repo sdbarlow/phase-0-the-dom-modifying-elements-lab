c8f3ba21b8864fd3eded8ec87a9ed1783ed1cc9d
Seth Barlow <sethdbarlow@gmail.com>
2023-01-31 19:43:21 -0700

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const mainRem = document.querySelector('main#main').remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id='victory';
+
+newHeader.textContent="Seth is the champion"
